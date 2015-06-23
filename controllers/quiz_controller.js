796e9cd40f1a7e936c652f76873b780d096698f5
Miguel Angel Romero <miguelangelromero@users.noreply.github.com>
2015-06-24 01:56:00 +0200

    Commit de rama busquedas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -12,11 +12,21 @@ exports.load = function(req, res, next, quizId) {
 	}).catch(function(error){ next(error);});
 };
 
-// GET /quizes
+
+// GET con parametro de busqueda de preguntas /quizes?search=
 exports.index = function (req,res) {
-	models.Quiz.findAll().then(function(quizes){
+	if(req.query.search){ 
+	  var buscar = req.query.search.replace(/\s/g,"%");
+console.log("-->"+buscar);
+	  models.Quiz.findAll({where:["pregunta like ?", "%"+buscar+"%"], order: "pregunta ASC"})
+	    .then(function(quizes){
 		res.render('quizes/index', {quizes: quizes, title: 'Quiz'});
-	})
+	  })
+	} else {
+	  models.Quiz.findAll().then(function(quizes){
+		res.render('quizes/index', {quizes: quizes, title: 'Quiz'});
+	  })
+	}
 };
 
 // GET /quizes/:id
--- models/models.js
+++ models/models.js
@@ -41,6 +41,15 @@ sequelize.sync().then(function() {
 			Quiz.create({ pregunta: "Capital de Italia",
 				      respuesta: "Roma"
 				    });
+			Quiz.create({ pregunta: "Capital de España",
+				      respuesta: "Madrid"
+				    });
+			Quiz.create({ pregunta: "Capital de Rusia",
+				      respuesta: "Moscú"
+				    });
+			Quiz.create({ pregunta: "Capital de Francia",
+				      respuesta: "Paris"
+				    });
 			Quiz.create({ pregunta: "Capital de Portugal",
 				      respuesta: "Lisboa"
 				    })
