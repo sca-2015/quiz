61ea82bfc6ee82dbf30cddc46d7b4a61c8d76dd8
Miguel Angel Romero <miguelangelromero@users.noreply.github.com>
2015-06-25 00:13:50 +0200

    Crear pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -17,7 +17,6 @@ exports.load = function(req, res, next, quizId) {
 exports.index = function (req,res) {
 	if(req.query.search){ 
 	  var buscar = req.query.search.replace(/\s/g,"%");
-console.log("-->"+buscar);
 	  models.Quiz.findAll({where:["pregunta like ?", "%"+buscar+"%"], order: "pregunta ASC"})
 	    .then(function(quizes){
 		res.render('quizes/index', {quizes: quizes, title: 'Quiz'});
@@ -41,6 +40,25 @@ exports.answer = function (req,res) {
 	} else {
 		res.render('quizes/answer', {quiz: req.quiz, respuesta: 'Incorrecto', title: 'Quiz'});
 	}
+};
+
+// GET /quizes/new
+exports.new = function(req, res) {
+  var quiz = models.Quiz.build(
+    {pregunta: "Pregunta", respuesta: "Respuesta"}
+  );
+
+  res.render('quizes/new', {quiz: quiz});
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+  var quiz = models.Quiz.build( req.body.quiz );
+
+// guarda en DB los campos pregunta y respuesta de quiz
+  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+    res.redirect('/quizes');  
+  })   // res.redirect: Redirección HTTP a lista de preguntas
 };
 
 // GET /quizes/author
